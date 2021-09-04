1e364d5e50bfa075e4d2c6115a7981e33a7e7681
Milos Bazelides <37623188+mbaze@users.noreply.github.com>
2021-09-04 20:46:14 +0200

    Added experimental E1R1 decoder.

---
+++ asm/Z80/experimental/DecodeE1R1.asm
@@ -0,0 +1,44 @@
+; Copyright (c) 2021, Milos "baze" Bazelides
+; This code is released under the terms of the BSD 2-Clause License.
+
+; E1R1 decoder.
+
+; The decoder assumes reverse order. We can omit the end of stream
+; marker if we let the last literal overwrite opcodes after LDDR.
+
+		ld	hl,SrcAddr
+		ld	de,DstAddr
+		ld	bc,#FF00	; Ideally, these values should be "reused"
+		ld	a,%11000000	; e.g. by aligning the addresses.
+
+EliasLength	add	a,a
+		rl	c
+;		ret	c		; Option to include the end of stream marker.
+NextBit		add	a,a
+		jr	nz,NoFetch
+		ld	a,(hl)
+		dec	hl
+		rla
+NoFetch		jr	c,EliasLength
+		rla
+		jr	nc,LoadOffset
+		inc	b		; Was it a phrase?
+		jr	z,CopyBytes
+		dec	b
+		ex	af,af'
+		jr	ReuseOffset
+LoadOffset	push	hl
+		ex	af,af'
+		ld	a,(hl)
+ReuseOffset	ld	l,a
+		ex	af,af'
+		ld	h,b
+		add	hl,de
+;		inc	hl		; Option to increase offset to 256.
+		inc	c
+CopyBytes	lddr
+		inc	c
+		jr	c,NextBit
+		pop	hl
+		dec	hl
+		djnz	NextBit		; Set B = 255 to indicate phrase.

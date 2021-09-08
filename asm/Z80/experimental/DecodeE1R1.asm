da2db7ef73c45d0de79ce6909e8a7fa0947f8559
Milos Bazelides <37623188+mbaze@users.noreply.github.com>
2021-09-08 15:32:13 +0200

    Update DecodeE1R1.asm

--- asm/Z80/experimental/DecodeE1R1.asm
+++ asm/Z80/experimental/DecodeE1R1.asm
@@ -1,7 +1,7 @@
 ; Copyright (c) 2021, Milos "baze" Bazelides
 ; This code is released under the terms of the BSD 2-Clause License.
 
-; E1R1 decoder (39 bytes excluding initialization).
+; E1R1 decoder (40 bytes excluding initialization).
 
 ; The decoder assumes reverse order. We can omit the end of stream
 ; marker if we let the last literal overwrite opcodes after LDDR.
@@ -20,14 +20,15 @@ NextBit		add	a,a
 		dec	hl
 		rla
 NoFetch		jr	c,EliasLength
-		push	hl
 		rla
 		jr	nc,LoadOffset
 		inc	b		; Was it a phrase?
 		jr	z,CopyBytes
+		push	hl
 		ex	af,af'
 		jr	ReuseOffset
-LoadOffset	ex	af,af'
+LoadOffset	push	hl
+		ex	af,af'
 		ld	a,(hl)
 ReuseOffset	ld	l,a
 		ex	af,af'
@@ -37,8 +38,8 @@ ReuseOffset	ld	l,a
 ;		inc	hl		; Option to increase offset to 256.
 		inc	c
 CopyBytes	lddr
-		pop	hl
 		inc	c
 		jr	c,NextBit
+		pop	hl
 		dec	hl
 		djnz	NextBit		; Set B = 255 to indicate phrase.

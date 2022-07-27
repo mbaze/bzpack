ae3593e5fcc700436c901ea6f4e005be98f30ffd
Milos Bazelides <37623188+mbaze@users.noreply.github.com>
2022-07-27 20:09:22 +0200

    Update DecodeE1ZX.asm

--- asm/Z80/DecodeE1ZX.asm
+++ asm/Z80/DecodeE1ZX.asm
@@ -1,10 +1,10 @@
 ; Copyright (c) 2022, Milos "baze" Bazelides
 ; This code is released under the terms of the BSD 2-Clause License.
 
-; E1ZX decoder (28 bytes excluding initialization, usually 35 bytes including initialization, 36 bytes max).
+; E1ZX decoder (28 bytes excluding initialization, usually 35 bytes including initialization, 39 bytes max).
 
-; The decoder assumes reverse order. The end of stream marker can be omitted
-; if we let the last literal overwrite opcodes after LDDR and let the code continue.
+; The decoder assumes reverse order. End of stream marker is not supported.
+; The last literal is expected to overwrite opcodes after LDDR.
 
 		ld	a,128		; Note that the ZX Spectrum's USR function sets A = C upon entry.
 		ld	c,a		; If the start address is #xx80 both registers already equal 128.

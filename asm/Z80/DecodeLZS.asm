a60299940e51dd7008532185189c57a1de688d13
Milos Bazelides <37623188+mbaze@users.noreply.github.com>
2022-07-28 14:34:57 +0200

    Optimized LZS decoder for ZX Spectrum (24 bytes).

--- asm/Z80/DecodeLZS.asm
+++ asm/Z80/DecodeLZS.asm
@@ -6,6 +6,7 @@
 ; The decoder assumes reverse order. The end of stream marker can be omitted
 ; if we let the last literal overwrite opcodes after LDDR and let the code continue.
 
+IF 0
 		ld	hl,SrcAddr
 		ld	de,DstAddr
 		ld	b,0		; Ideally this value should be "reused".
@@ -26,3 +27,31 @@ CopyBytes	lddr
 		pop	hl
 		dec	hl
 		jr	MainLoop
+ELSE
+
+; On Sinclair ZX Spectrum we can make a couple of assumptions. If the start address is #XX00
+; the USR function will set BC accordingly. Also, if the input stream is placed directly
+; before the entry point we can make the decompressor only 24 bytes long using pre-decrement.
+
+		ld	h,b
+		ld	l,c
+		ld	b,c
+		ld	de,DstAddr
+
+MainLoop	dec	hl
+		ld	c,(hl)
+		srl	c
+;		ret	z		; Option to include the end of stream marker.
+;		inc	c		; Option to increase length to 128.
+		jr	c,CopyBytes
+		dec	hl
+		push	hl
+		ld	l,(hl)
+		ld	h,b
+		add	hl,de
+;		inc	hl		; Option to increase offset to 256.
+CopyBytes	lddr
+		jr	c,MainLoop
+		pop	hl
+		jr	MainLoop
+ENDIF

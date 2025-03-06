5c9f923951b110b0f29b7ef3d97f99f2fa85ac32
mbaze <baze@baze.sk>
2025-03-06 21:46:35 +0100

    Update build.bat

--- llvm/build.bat
+++ llvm/build.bat
@@ -1,9 +1,1 @@
-clang++ -std=c++14 -O3 -o ../bin/bzpack.exe ^
-../src/BitStream.cpp ^
-../src/Compressor.cpp ^
-../src/Decompressor.cpp ^
-../src/DijkstraParser.cpp ^
-../src/Main.cpp ^
-../src/OptimalParser.cpp ^
-../src/PrefixMatcher.cpp ^
-../src/UniversalCodes.cpp
+clang++ -std=c++14 -O3 -o ../bin/bzpack.exe ../src/*.cpp

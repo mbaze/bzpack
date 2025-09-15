84c82d63e85eb538fc14094b1de7f7ce5e6dced6
mbaze <baze@baze.sk>
2025-09-15 16:10:57 +0200

    Updated clang.

--- llvm/build.bat
+++ llvm/build.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin\clang++" -std=c++14 -O3 -o ../bin/bzpack.exe ../src/*.cpp
+clang++ -std=c++14 -O3 -o ../bin/bzpack.exe ../src/*.cpp
